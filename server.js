2865b49218063a4c76991bc947c64e317879e4aa
Pablo Romanelli <pabloromanelli@gmail.com>
2018-02-23 11:37:12 -0300

    Improved body parsing validation

--- server.js
+++ server.js
@@ -10,13 +10,15 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
+  if (!req.body || (Object.keys(req.body).length === 0 && req.body.constructor === Object))
+    return res.sendStatus(400)
+
   const result = ST
     .select(req.body.template)
     .transform(req.body.data)
     .root();
 
   res.json(result);
-  console.log('')
 });
 
 const PORT = 80;
